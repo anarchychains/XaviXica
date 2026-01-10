987be0d983428eb37c4999e7b34757c8ff0804a6
Uai so serious? <103005830+anarchychains@users.noreply.github.com>
2026-01-10 20:02:36 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import AppShell from "./components/AppShell";
 import CryptoContentAgent from "./CryptoContentAgent";
 
 export default function App() {
-  return <CryptoContentAgent />;
+  return (
+    <AppShell>
+      <CryptoContentAgent />
+    </AppShell>
+  );
 }
